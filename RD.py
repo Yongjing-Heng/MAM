f72ef90788b664dd8f31e3aa512a0c80dc5f2eca
Weiyu Zeng <46858795+weiyu-zeng@users.noreply.github.com>
2019-10-31 19:04:42 +0800

    Add files via upload

---
+++ RD.py
@@ -0,0 +1,314 @@
+import numpy as np
+import random
+from PIL import Image
+
+
+
+class RandomDisrupt(object):
+    def __init__(self, cutting_num, p=0.5):
+        self.cutting_num = cutting_num
+        self.p = p
+
+    def __call__(self, img):
+        return disrupt_image(img, self.cutting_num, self.p)
+
+
+def disrupt_image(img, cutting_num, p):
+    imgnp = np.array(img)
+    if img.mode == 'L':
+        h, w = imgnp.shape
+    if img.mode == 'RGB' or img.mode == 'RGBA':
+        h, w, c = imgnp.shape
+
+    a = random.random()
+
+    """
+    2 slices cutting
+    """
+    if cutting_num == 2 and a <= p:
+        if a <= p / 2:
+            up_half = imgnp[0:h // 2 + 1, 0:w]
+            down_half = imgnp[h // 2 + 1:h, 0:w]
+            imgnp = np.vstack((down_half, up_half))
+        else:
+            left_half = imgnp[0:h, 0:w // 2 + 1]
+            right_half = imgnp[0:h, w // 2 + 1:w]
+            imgnp = np.hstack((right_half, left_half))
+
+    """
+    3 slices cutting
+    """
+    if cutting_num == 3 and a <= p:
+        if a <= p / 2:
+            a_piece = imgnp[0:h // 3 + 1, 0:w]
+            b_piece = imgnp[h // 3 + 1:2 * h // 3 + 1, 0:w]
+            c_piece = imgnp[2 * h // 3 + 1:h, 0:w]
+            if a <= p / 10:
+                imgnp = np.vstack((c_piece, b_piece, a_piece))
+            elif p / 10 < a <= 2 * p / 10:
+                imgnp = np.vstack((c_piece, a_piece, b_piece))
+            elif 2 * p / 10 < a <= 3 * p / 10:
+                imgnp = np.vstack((a_piece, c_piece, b_piece))
+            elif 3 * p / 10 < a <= 4 * p / 10:
+                imgnp = np.vstack((b_piece, a_piece, c_piece))
+            else:
+                imgnp = np.vstack((b_piece, c_piece, a_piece))
+
+        else:
+            a_piece = imgnp[0:h, 0:w // 3 + 1]
+            b_piece = imgnp[0:h, w // 3 + 1:2 * w // 3 + 1]
+            c_piece = imgnp[0:h, 2 * w // 3 + 1:w]
+            if p / 2 < a <= 6 * p / 10:
+                imgnp = np.hstack((c_piece, b_piece, a_piece))
+            elif 6 * p / 10 < a <= 7 * p / 10:
+                imgnp = np.hstack((c_piece, a_piece, b_piece))
+            elif 7 * p / 10 < a <= 8 * p / 10:
+                imgnp = np.hstack((a_piece, c_piece, b_piece))
+            elif 8 * p / 10 < a <= 9 * p / 10:
+                imgnp = np.hstack((b_piece, a_piece, c_piece))
+            else:
+                imgnp = np.hstack((b_piece, c_piece, a_piece))
+
+    """
+    4 slices cutting
+    """
+    if cutting_num == 4 and a <= p:
+#        if h % 2 == 0 and w % 2 == 0: imgnp = four_cutting_v1(imgnp, h, w, a, p)  # cross cutting
+        imgnp = four_cutting_v2(imgnp, h, w, a, p)
+
+
+    if cutting_num != 3 and cutting_num != 4 and cutting_num != 2:
+        print()
+        print(" -- The module 'RandomDisrupt' didn't work , please pick a right number for 'cutting_num' -- ")
+
+    """
+    if gray
+    """
+    if img.mode == 'L':
+        img = Image.fromarray(imgnp.astype('uint8')).convert('L')
+        return img
+    """
+    if RGB
+    """
+    if img.mode == 'RGB' or img.mode == 'RGBA':
+        img = Image.fromarray(imgnp.astype('uint8')).convert('RGB')
+        return img
+
+
+"""
+cross cutting(fail)
+"""
+def four_cutting_v1(imgnp, h, w, a, p):
+    a_piece = imgnp[0:h // 2, 0:w // 2]
+    b_piece = imgnp[0:h // 2, w // 2:w]
+    c_piece = imgnp[h // 2:h, 0:w // 2]
+    d_piece = imgnp[h // 2:h, w // 2:w]
+
+    if a <= p / 23:
+        a_half = np.hstack((a_piece, b_piece))
+        b_half = np.hstack((d_piece, c_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif p / 23 < a <= 2 * p / 23:
+        a_half = np.hstack((a_piece, c_piece))
+        b_half = np.hstack((b_piece, d_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 2 * p / 23 < a <= 3 * p / 23:
+        a_half = np.hstack((a_piece, c_piece))
+        b_half = np.hstack((d_piece, b_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 3 * p / 23 < a <= 4 * p / 23:
+        a_half = np.hstack((a_piece, d_piece))
+        b_half = np.hstack((b_piece, c_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 4 * p / 23 < a <= 5 * p / 23:
+        a_half = np.hstack((a_piece, d_piece))
+        b_half = np.hstack((c_piece, b_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 5 * p / 23 < a <= 6 * p / 23:
+        a_half = np.hstack((b_piece, a_piece))
+        b_half = np.hstack((c_piece, d_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 6 * p / 23 < a <= 7 * p / 23:
+        a_half = np.hstack((b_piece, a_piece))
+        b_half = np.hstack((d_piece, c_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 7 * p / 23 < a <= 8 * p / 23:
+        a_half = np.hstack((b_piece, c_piece))
+        b_half = np.hstack((a_piece, d_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 8 * p / 23 < a <= 9 * p / 23:
+        a_half = np.hstack((b_piece, c_piece))
+        b_half = np.hstack((d_piece, a_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 9 * p / 23 < a <= 10 * p / 23:
+        a_half = np.hstack((b_piece, d_piece))
+        b_half = np.hstack((a_piece, c_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 10 * p / 23 < a <= 11 * p / 23:
+        a_half = np.hstack((b_piece, d_piece))
+        b_half = np.hstack((c_piece, a_piece))
+        imgnp = np.vstack((a_half, b_half))
+
+    elif 11 * p / 23 < a <= 12 * p / 23:
+        a_half = np.hstack((c_piece, a_piece))
+        b_half = np.hstack((b_piece, d_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 12 * p / 23 < a <= 13 * p / 23:
+        a_half = np.hstack((c_piece, a_piece))
+        b_half = np.hstack((d_piece, b_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 13 * p / 23 < a <= 14 * p / 23:
+        a_half = np.hstack((c_piece, b_piece))
+        b_half = np.hstack((a_piece, d_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 14 * p / 23 < a <= 15 * p / 23:
+        a_half = np.hstack((c_piece, b_piece))
+        b_half = np.hstack((d_piece, a_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 15 * p / 23 < a <= 16 * p / 23:
+        a_half = np.hstack((c_piece, d_piece))
+        b_half = np.hstack((a_piece, b_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 16 * p / 23 < a <= 17 * p / 23:
+        a_half = np.hstack((c_piece, d_piece))
+        b_half = np.hstack((b_piece, a_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 17 * p / 23 < a <= 18 * p / 23:
+        a_half = np.hstack((d_piece, a_piece))
+        b_half = np.hstack((b_piece, c_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 18 * p / 23 < a <= 19 * p / 23:
+        a_half = np.hstack((d_piece, a_piece))
+        b_half = np.hstack((c_piece, b_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 19 * p / 23 < a <= 20 * p / 23:
+        a_half = np.hstack((d_piece, b_piece))
+        b_half = np.hstack((a_piece, c_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 20 * p / 23 < a <= 21 * p / 23:
+        a_half = np.hstack((d_piece, b_piece))
+        b_half = np.hstack((c_piece, a_piece))
+        imgnp = np.vstack((a_half, b_half))
+    elif 21 * p / 23 < a <= 22 * p / 23:
+        a_half = np.hstack((d_piece, c_piece))
+        b_half = np.hstack((a_piece, b_piece))
+        imgnp = np.vstack((a_half, b_half))
+    else:
+        a_half = np.hstack((d_piece, c_piece))
+        b_half = np.hstack((b_piece, a_piece))
+        imgnp = np.vstack((a_half, b_half))
+
+    return imgnp
+
+
+"""
+single cutting direction
+"""
+def four_cutting_v2(imgnp, h, w, a, p):
+    if a <= p / 2:
+        a_piece = imgnp[0:h // 4 + 1, 0:w]
+        b_piece = imgnp[h // 4 + 1:2 * h // 4 + 1, 0:w]
+        c_piece = imgnp[2 * h // 4 + 1:3 * h // 4 + 1, 0:w]
+        d_piece = imgnp[3 * h // 4 + 1:h, 0:w]
+        if a <= p / 47:
+            imgnp = np.vstack((a_piece, b_piece, d_piece, c_piece))
+        elif p / 47 < a <= 2 * p / 47:
+            imgnp = np.vstack((a_piece, c_piece, b_piece, d_piece))
+        elif 2 * p / 47 < a <= 3 * p / 47:
+            imgnp = np.vstack((a_piece, c_piece, d_piece, b_piece))
+        elif 3 * p / 47 < a <= 4 * p / 47:
+            imgnp = np.vstack((a_piece, d_piece, b_piece, c_piece))
+        elif 4 * p / 47 < a <= 5 * p / 47:
+            imgnp = np.vstack((a_piece, d_piece, c_piece, b_piece))
+        elif 5 * p / 47 < a <= 6 * p / 47:
+            imgnp = np.vstack((b_piece, a_piece, c_piece, d_piece))
+        elif 6 * p / 47 < a <= 7 * p / 47:
+            imgnp = np.vstack((b_piece, a_piece, d_piece, c_piece))
+        elif 7 * p / 47 < a <= 8 * p / 47:
+            imgnp = np.vstack((b_piece, c_piece, a_piece, d_piece))
+        elif 8 * p / 47 < a <= 9 * p / 47:
+            imgnp = np.vstack((b_piece, c_piece, d_piece, a_piece))
+        elif 9 * p / 47 < a <= 10 * p / 47:
+            imgnp = np.vstack((b_piece, d_piece, a_piece, c_piece))
+        elif 10 * p / 47 < a <= 11 * p / 47:
+            imgnp = np.vstack((b_piece, d_piece, c_piece, a_piece))
+        elif 11 * p / 47 < a <= 12 * p / 47:
+            imgnp = np.vstack((c_piece, a_piece, b_piece, d_piece))
+        elif 12 * p / 47 < a <= 13 * p / 47:
+            imgnp = np.vstack((c_piece, a_piece, d_piece, b_piece))
+        elif 13 * p / 47 < a <= 14 * p / 47:
+            imgnp = np.vstack((c_piece, b_piece, a_piece, d_piece))
+        elif 14 * p / 47 < a <= 15 * p / 47:
+            imgnp = np.vstack((c_piece, b_piece, d_piece, a_piece))
+        elif 15 * p / 47 < a <= 16 * p / 47:
+            imgnp = np.vstack((c_piece, d_piece, a_piece, b_piece))
+        elif 16 * p / 47 < a <= 17 * p / 47:
+            imgnp = np.vstack((c_piece, d_piece, b_piece, a_piece))
+        elif 17 * p / 47 < a <= 18 * p / 47:
+            imgnp = np.vstack((d_piece, a_piece, b_piece, c_piece))
+        elif 18 * p / 47 < a <= 19 * p / 47:
+            imgnp = np.vstack((d_piece, a_piece, c_piece, b_piece))
+        elif 19 * p / 47 < a <= 20 * p / 47:
+            imgnp = np.vstack((d_piece, b_piece, a_piece, c_piece))
+        elif 20 * p / 47 < a <= 21 * p / 47:
+            imgnp = np.vstack((d_piece, b_piece, c_piece, a_piece))
+        elif 21 * p / 47 < a <= 22 * p / 47:
+            imgnp = np.vstack((d_piece, c_piece, a_piece, b_piece))
+        else:
+            imgnp = np.vstack((d_piece, c_piece, b_piece, a_piece))
+
+    if p / 2 < a:
+        a_piece = imgnp[0:h, 0:w // 4 + 1]
+        b_piece = imgnp[0:h, w // 4 + 1:2 * w // 4 + 1]
+        c_piece = imgnp[0:h, 2 * w // 4 + 1:3 * w // 4 + 1]
+        d_piece = imgnp[0:h, 3 * w // 4 + 1:w]
+        if 23 * p / 47 < a <= 24 * p / 47:
+            imgnp = np.hstack((a_piece, b_piece, c_piece, d_piece))
+        elif 24 * p / 47 < a <= 25 * p / 47:
+            imgnp = np.hstack((a_piece, b_piece, d_piece, c_piece))
+        elif 25 * p / 47 < a <= 26 * p / 47:
+            imgnp = np.hstack((a_piece, c_piece, b_piece, d_piece))
+        elif 26 * p / 47 < a <= 27 * p / 47:
+            imgnp = np.hstack((a_piece, c_piece, d_piece, b_piece))
+        elif 27 * p / 47 < a <= 28 * p / 47:
+            imgnp = np.hstack((a_piece, d_piece, b_piece, c_piece))
+        elif 28 * p / 47 < a <= 29 * p / 47:
+            imgnp = np.hstack((a_piece, d_piece, c_piece, b_piece))
+        elif 29 * p / 47 < a <= 30 * p / 47:
+            imgnp = np.hstack((b_piece, a_piece, c_piece, d_piece))
+        elif 30 * p / 47 < a <= 31 * p / 47:
+            imgnp = np.hstack((b_piece, a_piece, d_piece, c_piece))
+        elif 31 * p / 47 < a <= 32 * p / 47:
+            imgnp = np.hstack((b_piece, c_piece, a_piece, d_piece))
+        elif 32 * p / 47 < a <= 33 * p / 47:
+            imgnp = np.hstack((b_piece, c_piece, d_piece, a_piece))
+        elif 33 * p / 47 < a <= 34 * p / 47:
+            imgnp = np.hstack((b_piece, d_piece, a_piece, c_piece))
+        elif 34 * p / 47 < a <= 35 * p / 47:
+            imgnp = np.hstack((b_piece, d_piece, c_piece, a_piece))
+        elif 35 * p / 47 < a <= 36 * p / 47:
+            imgnp = np.hstack((c_piece, a_piece, b_piece, d_piece))
+        elif 36 * p / 47 < a <= 37 * p / 47:
+            imgnp = np.hstack((c_piece, a_piece, d_piece, b_piece))
+        elif 37 * p / 47 < a <= 38 * p / 47:
+            imgnp = np.hstack((c_piece, b_piece, a_piece, d_piece))
+        elif 38 * p / 47 < a <= 39 * p / 47:
+            imgnp = np.hstack((c_piece, b_piece, d_piece, a_piece))
+        elif 39 * p / 47 < a <= 40 * p / 47:
+            imgnp = np.hstack((c_piece, d_piece, a_piece, b_piece))
+        elif 40 * p / 47 < a <= 41 * p / 47:
+            imgnp = np.hstack((c_piece, d_piece, b_piece, a_piece))
+        elif 41 * p / 47 < a <= 42 * p / 47:
+            imgnp = np.hstack((d_piece, a_piece, b_piece, c_piece))
+        elif 42 * p / 47 < a <= 43 * p / 47:
+            imgnp = np.hstack((d_piece, a_piece, c_piece, b_piece))
+        elif 43 * p / 47 < a <= 44 * p / 47:
+            imgnp = np.hstack((d_piece, b_piece, a_piece, c_piece))
+        elif 44 * p / 47 < a <= 45 * p / 47:
+            imgnp = np.hstack((d_piece, b_piece, c_piece, a_piece))
+        elif 45 * p / 47 < a <= 46 * p / 47:
+            imgnp = np.hstack((d_piece, c_piece, a_piece, b_piece))
+        else:
+            imgnp = np.hstack((d_piece, c_piece, b_piece, a_piece))
+
+    return imgnp
---
+++ demo.py
@@ -0,0 +1,232 @@
+import torch as t
+import torchvision as tv
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import torch.utils.data
+import datetime
+import argparse
+
+from basic_layers import ResidualBlock
+from RD import RandomDisrupt
+from Mixed_Attention_Module import MixedAttentionBlock
+
+
+WORKERS = 4
+PARAS_FN = 'cifar10_mixed_attention_module.pkl'
+ROOT = 'E:/cifar10'
+loss_func = nn.CrossEntropyLoss()
+best_acc = 0
+global_train_error = []
+global_test_error = []
+
+
+
+
+class MixedAttentionModel(nn.Module):
+    def __init__(self):
+        super(MixedAttentionModel, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True)
+        )
+        self.residual_block1 = MixedAttentionBlock(32, 128)  # 32*32
+        self.residual_block2 = MixedAttentionBlock(128, 256, 2)  # 16*16
+        self.residual_block3 = MixedAttentionBlock(256, 512, 2)  # 4*4
+        self.residual_block4 = MixedAttentionBlock(512, 1024)  # 8*8
+        self.residual_block5 = MixedAttentionBlock(1024, 1024)  # 8*8
+        self.residual_block6 = MixedAttentionBlock(1024, 1024)  # 8*8
+        self.mpool2 = nn.Sequential(
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=8)
+        )
+        self.fc = nn.Linear(1024,10)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.residual_block1(out)
+        out = self.residual_block2(out)
+        out = self.residual_block3(out)
+        out = self.residual_block4(out)
+        out = self.residual_block5(out)
+        out = self.residual_block6(out)
+        out = self.mpool2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
+
+
+
+
+def net_train(net, train_data_load, optimizer, epoch, log_interval):
+    net.train()
+    begin = datetime.datetime.now()
+    total = len(train_data_load.dataset)
+    train_loss = 0
+    ok = 0
+
+    for i, data in enumerate(train_data_load, 0):
+        img, label = data
+        img, label = img.cuda(), label.cuda()
+        optimizer.zero_grad()
+        outs = net(img)
+        loss = loss_func(outs, label)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        _, predicted = t.max(outs.data, 1)
+        ok += (predicted == label).sum()
+
+        if (i + 1) % log_interval == 0:
+            loss_mean = train_loss / (i + 1)
+            traind_total = (i + 1) * len(label)
+            acc = 100. * ok / traind_total
+            progress = 100. * traind_total / total
+            print('Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}  Acc: {:.6f}'.format(
+                epoch, traind_total, total, progress, loss_mean, acc))
+
+    error = (100 - acc).cpu().numpy()
+    global_train_error.append(float("%.2f" % error))
+
+    end = datetime.datetime.now()
+    print('time spend: ', end - begin)
+
+
+
+def net_test(net, test_data_load, epoch):
+    net.eval()
+    ok = 0
+    test_loss = 0
+    for i, data in enumerate(test_data_load):
+        img, label = data
+        img, label = img.cuda(), label.cuda()
+        outs = net(img)
+        loss = loss_func(outs, label)
+        test_loss += loss.item()
+        _, pre = t.max(outs.data, 1)
+        ok += (pre == label).sum()
+
+    acc = ok.item() * 100. / (len(test_data_load.dataset))
+    print('EPOCH:{}, ACC:{}\n'.format(epoch, acc))
+    global_test_error.append(round((100 - acc), 2))
+    global best_acc
+    if acc > best_acc:
+        best_acc = acc
+
+
+
+
+def show_error_curv():
+    train_x = list(range(len(global_train_error)))
+    train_y = global_train_error
+    test_x = list(range(len(global_test_error)))
+    test_y = global_test_error
+
+    plt.title('CIFAR10 MAM ERROR')
+    plt.plot(train_x, train_y, color='green', label='training error')
+    plt.plot(test_x, test_y, color='red', label='testing error')
+    plt.legend()
+    plt.xlabel('iterations')
+    plt.ylabel('error %')
+    plt.show()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='PyTorch CIFA10 Mixed Attention Module')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--test-batch-size', type=int, default=20, metavar='N',
+                        help='input batch size for testing (default: 100)')
+    parser.add_argument('--epochs', type=int, default=200, metavar='N',
+                        help='number of epochs to train (default: 200)')
+    parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                        help='learning rate (default: 0.1)')
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                        help='SGD momentum (default: 0.9)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status (default: 10)')
+    parser.add_argument('--no-train', action='store_true', default=False,
+                        help='If train the Model')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+
+    transform_train = tv.transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop((32, 32), padding=4),
+
+#        RandomDisrupt(2),
+
+        transforms.ToTensor(),
+    ])
+
+    transform_test = tv.transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_data = tv.datasets.CIFAR10(root=ROOT, train=True, download=True, transform=transform_train)
+    test_data = tv.datasets.CIFAR10(root=ROOT, train=False, download=False, transform=transform_test)
+    train_load = t.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=WORKERS)
+    test_load = t.utils.data.DataLoader(test_data, batch_size=args.test_batch_size, shuffle=False, num_workers=WORKERS)
+
+    net = MixedAttentionModel()
+    net = net.cuda()
+    print(net)
+
+#    net = nn.DataParallel(net)
+#    cudnn.benchmark = True
+
+    if args.no_train:
+        net.load_state_dict(t.load(PARAS_FN))
+        net_test(net, test_load, 0)
+        return
+
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, nesterov=True, weight_decay=0.0001)
+    start_time = datetime.datetime.now()
+
+    for epoch in range(1, args.epochs + 1):
+        net_train(net, train_load, optimizer, epoch, args.log_interval)
+        net_test(net, test_load, epoch)
+
+        if (epoch+1) == 60:    ##################################3
+            lr = args.lr / 10
+            print('reset learning rate to:', lr)
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+                print(param_group['lr'])
+
+        if (epoch + 1) == 120:  ##################################3
+            lr = args.lr / 100
+            print('reset learning rate to:', lr)
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+                print(param_group['lr'])
+
+        if (epoch + 1) == 180:  ##################################3
+            lr = args.lr / 1000
+            print('reset learning rate to:', lr)
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+                print(param_group['lr'])
+
+    end_time = datetime.datetime.now()
+
+    print("global train error: \n", global_train_error)
+    print()
+    print("global test error: \n", global_test_error)
+
+    global best_acc
+    print('CIFAR10 Mixed Attention Network: EPOCH:{}, BATCH_SZ:{}, LR:{}, ACC:{}'.format(args.epochs, args.batch_size, args.lr, best_acc))
+    print('train spend time: ', end_time - start_time)
+
+    show_error_curv()
+
+    if args.save_model:
+        t.save(net.state_dict(), PARAS_FN)
+
+if __name__ == '__main__':
+    main()

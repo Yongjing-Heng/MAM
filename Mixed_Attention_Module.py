30d809b55a75783a19413bd30c03dd3379784003
Weiyu Zeng <46858795+weiyu-zeng@users.noreply.github.com>
2019-11-03 15:54:48 +0800

    Update Mixed_Attention_Module.py

--- Mixed_Attention_Module.py
+++ Mixed_Attention_Module.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-
 class MixedAttentionBlock(nn.Module):
     def __init__(self, input_channels, output_channels, stride=1):
         super(MixedAttentionBlock, self).__init__()
@@ -17,10 +16,10 @@ class MixedAttentionBlock(nn.Module):
         self.bn3 = nn.BatchNorm2d(output_channels//4)
         self.relu = nn.ReLU(inplace=True)
         self.conv3 = nn.Conv2d(output_channels//4, output_channels, 1, 1, bias = False)
+        self.ca = ChannelAttention(output_channels)
+        self.sa = SpatialAttention()        
         self.conv4 = nn.Conv2d(input_channels, output_channels , 1, stride, bias = False)
 
-        self.ca = ChannelAttention(output_channels)
-        self.sa = SpatialAttention()
 
     def forward(self, x):
         residual = x
@@ -33,7 +32,6 @@ class MixedAttentionBlock(nn.Module):
         out = self.bn3(out)
         out = self.relu(out)
         out = self.conv3(out)
-
         out = self.ca(out) * out
         out = self.sa(out) * out
 
@@ -43,17 +41,14 @@ class MixedAttentionBlock(nn.Module):
         return out
 
 
-
 class ChannelAttention(nn.Module):
     def __init__(self, in_planes, ratio=16):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
-
         self.fc1   = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
         self.relu1 = nn.ReLU()
         self.fc2   = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
-
         self.sigmoid = nn.Sigmoid()
         self.tanh = nn.Tanh()
 
@@ -61,11 +56,9 @@ class ChannelAttention(nn.Module):
         avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
         max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
         min_out = self.fc2(self.relu1(self.fc1(-self.max_pool(-x))))
-
         out = self.sigmoid(avg_out + max_out) + self.tanh(min_out)
 
         return out
-
 
 
 class SpatialAttention(nn.Module):
@@ -88,12 +81,11 @@ class SpatialAttention(nn.Module):
         max_out, _ = torch.max(x, dim=1, keepdim=True)
         x1 = torch.cat([avg_out, max_out], dim=1)
         x1 = self.sigmoid(self.conv1(x1))
-
+        
         min_out, _ = torch.min(x, dim=1, keepdim=True)
         min_out = self.relu(self.deconv(min_out))  # >>> 1x2hx2w
         min_out = self.conv2(min_out)
         min_out = -self.maxpooling(-min_out)  # >>> 1xhxw
-
         x2 = self.tanh(min_out)
 
         return x1 + x2
